ab75345f7861bc3a73f42082cf2d6b2c787248ce
Strasse <jeka_krsk@mail.ru>
2023-11-26 20:54:15 +0700

    CRUD v.2 commit

--- 3.2-crud/stocks_products/logistic/views.py
+++ 3.2-crud/stocks_products/logistic/views.py
@@ -3,14 +3,32 @@ from rest_framework.viewsets import ModelViewSet
 from logistic.models import Product, Stock
 from logistic.serializers import ProductSerializer, StockSerializer
 
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
+from django_filters import rest_framework as filters
+from rest_framework.pagination import LimitOffsetPagination
+
+
+class StockFilter(filters.FilterSet):
+    products = filters.NumberFilter("positions__product")
+
+    class Meta:
+        model = Stock
+        fields = ['id', 'address']
 
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     # при необходимости добавьте параметры фильтрации
-
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_fields = ['title',]
+    search_fields = ['title', 'description']
 
 class StockViewSet(ModelViewSet):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
     # при необходимости добавьте параметры фильтрации
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_class = StockFilter
+    search_fields = ["positions__product__title", "positions__product__description"]
+    pagination_class = LimitOffsetPagination
